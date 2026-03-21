793a0141782b9f2ded8c82504e7a7fd4b7aaf6f8
Vinka Alrezky <vinka.aeris@gmail.com>
2026-03-21 21:18:31 +0800

    feat: add emulator to allowed host

--- resqlink/settings.py
+++ resqlink/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-ezc*l!514fjl)il+)c*r8$^j7*-cm7598ughw0yr_8)v^3r5bk
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "10.0.2.2"]
 
 
 # Application definition
