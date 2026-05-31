07a75fb5d3cddb95b5fdd7acf8f207a5c8a90ff8
adipppp <adhif2004@gmail.com>
2026-06-01 00:08:09 +0700

    refactor(maps): decouple map types and interface from Google Platform

--- backend/src/repositories/maps.ts
+++ backend/src/repositories/maps.ts
@@ -1,15 +1,12 @@
-import {
-  GoogleDirectionsResponse,
-  GoogleDistanceMatrixResponse,
-} from "../types";
+import { DirectionsResponse, DistanceMatrixResponse } from "../types";
 
 export interface IMapsRepository {
   getDistanceMatrix(
     origins: string[],
     destinations: string[],
-  ): Promise<GoogleDistanceMatrixResponse>;
+  ): Promise<DistanceMatrixResponse>;
   getDirections(
     origin: string,
     destination: string,
-  ): Promise<GoogleDirectionsResponse>;
+  ): Promise<DirectionsResponse>;
 }
